804317062a4553303a7186a1a532bcb1f568af79
tor.helge.skei <tor.helge.skei@gmail.com>
2021-08-24 00:34:28 +0200

    bitmap and surface

--- src/gui/widgets/kode_image_widget.h
+++ src/gui/widgets/kode_image_widget.h
@@ -2,6 +2,10 @@
 #define kode_image_widget_included
 //----------------------------------------------------------------------
 
+#define KODE_IMAGE_WIDGET_DRAW_NONE     0
+#define KODE_IMAGE_WIDGET_DRAW_SURFACE  1
+#define KODE_IMAGE_WIDGET_DRAW_BITMAP   2
+
 #include "gui//kode_image.h"
 #include "gui/widgets/kode_panel_widget.h"
 
@@ -12,15 +16,16 @@ class KODE_ImageWidget
 private:
 //------------------------------
 
-  bool MSurfaceAllocated = false;
 
 //------------------------------
 protected:
 //------------------------------
 
-  bool          MDrawImage        = true;
+  uint32_t      MDrawMode         = KODE_IMAGE_WIDGET_DRAW_SURFACE;
+  KODE_Bitmap*  MBitmap           = KODE_NULL;
   KODE_Surface* MSurface          = KODE_NULL;
-
+  bool          MBitmapAllocated  = false;
+  bool          MSurfaceAllocated = false;
 
 //------------------------------
 public:
@@ -36,6 +41,7 @@ public:
   //----------
 
   virtual ~KODE_ImageWidget() {
+    if (MBitmapAllocated && MBitmap) KODE_Delete MBitmap;
     if (MSurfaceAllocated && MSurface) KODE_Delete MSurface;
   }
 
@@ -43,37 +49,60 @@ public:
 public:
 //------------------------------
 
-  virtual void setDrawImage(bool ADraw) {
-    MDrawImage = ADraw;
+  /*
+    0 = none
+    1 = surface
+    2 = bitmap
+  */
+
+  virtual void setDrawMode(uint32_t AMode) {
+    MDrawMode = AMode;
   }
 
   //----------
 
+  virtual void setBitmap(KODE_Bitmap* ABitmap) {
+    MDrawMode = KODE_IMAGE_WIDGET_DRAW_BITMAP;
+    MBitmap = ABitmap;
+    MBitmapAllocated = false;
+  }
+
+
+  //----------
+
   virtual void setImage(KODE_Drawable* ATarget, KODE_Surface* ASurface) {
+    MDrawMode = KODE_IMAGE_WIDGET_DRAW_SURFACE;
     MSurface = ASurface;
     MSurfaceAllocated = false;
   }
 
 
   virtual void setImage(KODE_Drawable* ATarget, KODE_Image* AImage) {
+    MDrawMode = KODE_IMAGE_WIDGET_DRAW_SURFACE;
     MSurface = KODE_New KODE_Surface(ATarget,AImage->getWidth(),AImage->getHeight());
     MSurfaceAllocated = true;
     KODE_Painter* painter = KODE_New KODE_Painter(MSurface);
     painter->drawBitmap(0,0,AImage);
+    //painter->flush();
+    KODE_Delete painter;
   }
 
   //----------
 
   virtual void setImage(KODE_Drawable* ATarget, KODE_Bitmap* ABitmap) {
+    MDrawMode = KODE_IMAGE_WIDGET_DRAW_SURFACE;
     MSurface = KODE_New KODE_Surface(ATarget,ABitmap->getWidth(),ABitmap->getHeight());
     MSurfaceAllocated = true;
     KODE_Painter* painter = KODE_New KODE_Painter(MSurface);
     painter->uploadBitmap(0,0,ABitmap);
+    //painter->flush();
+    KODE_Delete painter;
   }
 
   //----------
 
   virtual void setImage(KODE_Drawable* ATarget, uint8_t* ABuffer, uint32_t ASize) {
+    MDrawMode = KODE_IMAGE_WIDGET_DRAW_SURFACE;
     KODE_Bitmap* bitmap = KODE_New KODE_Bitmap(ABuffer,ASize);
     bitmap->premultAlpha();
     setImage(ATarget,bitmap);
@@ -83,6 +112,7 @@ public:
   //----------
 
   virtual void setImage(KODE_Drawable* ATarget, const char* AFilename) {
+    MDrawMode = KODE_IMAGE_WIDGET_DRAW_SURFACE;
     KODE_Bitmap* bitmap = KODE_New KODE_Bitmap(AFilename);
     bitmap->premultAlpha();
     setImage(ATarget,bitmap);
@@ -92,14 +122,20 @@ public:
   //----------
 
   virtual void drawImage(KODE_BasePainter* APainter) {
-    if (MDrawImage) {
-      if (MSurface) {
-        KODE_FRect rect = KODE_FRect(0,0,MSurface->getWidth(),MSurface->getHeight());
-        APainter->drawBitmap(getRect().x,getRect().y,MSurface,rect);
-      }
-      //else if (MBitmap) {
-      //  APainter->uploadBitmap(getRect().x,getRect().y,MBitmap);
-      //}
+    switch (MDrawMode) {
+      case KODE_IMAGE_WIDGET_DRAW_NONE:
+        break;
+      case KODE_IMAGE_WIDGET_DRAW_SURFACE:
+        if (MSurface) {
+          KODE_FRect rect = KODE_FRect(0,0,MSurface->getWidth(),MSurface->getHeight());
+          APainter->drawBitmap(getRect().x,getRect().y,MSurface,rect);
+        }
+        break;
+      case KODE_IMAGE_WIDGET_DRAW_BITMAP:
+        if (MBitmap) {
+          APainter->uploadBitmap(getRect().x,getRect().y,MBitmap);
+        }
+        break;
     }
   }
 
