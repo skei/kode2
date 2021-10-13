d264de5fbfdab6575344c220660d39dcedd47448
tor.helge.skei <tor.helge.skei@gmail.com>
2021-10-14 01:24:24 +0200

    fixed int parameter steps

--- src/plugin/parameters/kode_int_parameter.h
+++ src/plugin/parameters/kode_int_parameter.h
@@ -11,12 +11,20 @@ class KODE_IntParameter
 
 public:
 
-  KODE_IntParameter(const char* AName, float AVal, float AMin, float AMax)
-  : KODE_FloatParameter(AName,AVal,AMin,AMax,1) {
+//  KODE_IntParameter(const char* AName, float AVal, float AMin, float AMax)
+//  : KODE_FloatParameter(AName,AVal,AMin,AMax,1) {
+//  }
+//
+//  KODE_IntParameter(const char* AName, const char* ALabel, float AVal, float AMin, float AMax)
+//  : KODE_FloatParameter(AName,ALabel,AVal,AMin,AMax,1) {
+//  }
+
+  KODE_IntParameter(const char* AName, int32_t AVal, int32_t AMin, int32_t AMax, int32_t AStep=1)
+  : KODE_FloatParameter(AName,AVal,AMin,AMax,AStep) {
   }
 
-  KODE_IntParameter(const char* AName, const char* ALabel, float AVal, float AMin, float AMax)
-  : KODE_FloatParameter(AName,ALabel,AVal,AMin,AMax,1) {
+  KODE_IntParameter(const char* AName, const char* ALabel, int32_t AVal, int32_t AMin, int32_t AMax, int32_t AStep=1)
+  : KODE_FloatParameter(AName,ALabel,AVal,AMin,AMax,AStep) {
   }
 
 public:
